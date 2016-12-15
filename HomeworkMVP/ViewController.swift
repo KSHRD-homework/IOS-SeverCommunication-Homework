1d82863708eefd9145b17da97017a549037778cd
Song Seaktheng <songseaktheng@Youvita-iMac.local>
2016-12-15 13:51:38 +0700

    Initial Commit

---
+++ HomeworkMVP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HomeworkMVP
+//
+//  Created by Song Seaktheng on 12/15/16.
+//  Copyright © 2016 Song Seaktheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
